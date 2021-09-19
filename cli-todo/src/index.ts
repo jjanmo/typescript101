9f66380771cdfdb2fc10aa42530e91420c544ef8
jjanmo <jjanmo07@hanmail.net>
2021-09-19 17:34:47 +0900

    feat: add/update todostyle using cli-table

--- cli-todo/src/index.ts
+++ cli-todo/src/index.ts
@@ -1,9 +1,12 @@
+import { Command, CommandPrintTodos } from './command';
 import { inputText } from './input';
 import Todo from './todo';
 import { Priority, Todos } from './type';
 
+const commands: Command[] = [new CommandPrintTodos()];
+
 async function init() {
-  const AppState: Todos = [
+  const todos: Todos = [
     new Todo('sample test1', 'This is sample test1', new Date(), new Date('2021-09-20'), 'Todo', Priority.High),
     new Todo('sample test2', 'This is sample test2', new Date(), new Date('2021-09-21'), 'Todo', Priority.Medium),
     new Todo('sample test3', 'This is sample test3', new Date(), new Date('2021-09-22'), 'Todo', Priority.Low),
@@ -11,8 +14,16 @@ async function init() {
 
   while (true) {
     console.clear();
+    commands.forEach(command => console.log(command.toString()));
+
+    console.log();
     const text = await inputText('원하는 명령을 입력하세요 : ');
     console.log('text : ', text);
+
+    const command = commands.find(command => command.key === text);
+    if (command) {
+      await command.run(todos);
+    }
   }
 }
 
--- cli-todo/src/todo.ts
+++ cli-todo/src/todo.ts
@@ -14,18 +14,37 @@ import { TodoState, Priority } from './type';
 export default class Todo {
   static nextId: number = 1;
   constructor(
-    private title: string,
-    private description: string,
-    private startDate: Date,
-    private endDate: Date,
-    private status: TodoState,
-    private priority: Priority,
+    private _title: string,
+    private _description: string,
+    private _startDate: Date,
+    private _endDate: Date,
+    private _status: TodoState,
+    private _priority: Priority,
     private _id: number = Todo.nextId
   ) {
     Todo.nextId++;
   }
 
+  // getter
   get id() {
     return this._id;
+  }
+  get title() {
+    return this._title;
+  }
+  get description() {
+    return this._description;
+  }
+  get startDate() {
+    return this._startDate;
+  }
+  get endDate() {
+    return this._endDate;
+  }
+  get status() {
+    return this._status;
+  }
+  get priority() {
+    return this._priority;
   }
 }
